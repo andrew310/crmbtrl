d9ab121c648412027bf13a0d25f37c7e28d48fc6
Andrew Brown <andrewbrown600@gmail.com>
2016-07-25 12:24:49 -0700

    babel and react added

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,12 @@
-console.log('react rocks!');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return (
+            <p>React Rocks!</p>
+        );
+    }
+}
+
+render(<App/>, document.getElementById('app'));
